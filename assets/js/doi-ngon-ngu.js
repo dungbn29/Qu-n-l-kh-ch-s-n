7e1f6ea21bc01bf9a2b50a2512bc3e86e3caa298
dungbn29 <tiendungchu298@gmail.com>
2023-05-22 08:06:43 +0700

    main

---
+++ assets/js/doi-ngon-ngu.js
@@ -0,0 +1,28 @@
+let currentLanguage = 'vi';
+
+const languageData = {
+  vi: {
+    toggleButton: 'VN',
+    logoImage: 'assets/images/co-vn.png'
+  },
+  en: {
+    toggleButton: 'EN',
+    logoImage: 'assets/images/co-anh.png'
+  }
+};
+
+function toggleLanguage() {
+  const toggleButton = document.getElementById('btnToggleLang');
+  const logoImage = document.getElementById('logo');
+  if (currentLanguage === 'vi') {
+    currentLanguage = 'en';
+    } else {
+    currentLanguage = 'vi'; 
+  }
+  toggleButton.textContent = languageData[currentLanguage].toggleButton;
+  logoImage.src = languageData[currentLanguage].logoImage;
+  logoImage.alt = "Logo " + (currentLanguage === 'vi' ? 'Tiếng Việt' : 'English');
+}
+
+const toggleButton = document.getElementById('btnToggleLang');
+toggleButton.addEventListener('click', toggleLanguage);
